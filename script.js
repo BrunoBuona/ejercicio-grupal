8b60658dc48744be02fa5c9950d74ea06207770a
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-10-04 21:48:58 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,42 @@
+*/
+let miNombre = "Mauro";
+
+//2.    Crear una variable llamada miApellido y guardar en ella su apellido.
+
+let miApellido = "Harretche";
+
+//3.    Crear una variable llamada miEdad y guardar en ella su edad.
+
+let miEdad = 24;
+
+//4.    Crear una variable llamada miMascota y guardar en ella el nombre de su mascota.
+
+let miMascota = "Dona"
+
+//5.    Crear una variable llamada edadMascota y guardar en ella la edad de la mascota.
+
+let edadMascota = 3;
+
+//6.    Visualizar todas las variables dentro de la consola del navegador escribiendo el nombre de cada una de ellas.
+
+console.log(miNombre)
+console.log(miApellido)
+console.log(miEdad)
+console.log(miMascota)
+console.log(edadMascota)
+
+//7.    Crear una variable llamada nombreCompleto y guardar en ella la concatenación de miNombre y miApellido.
+
+let miNombreCompleto = miNombre + " " + miApellido
+console.log(miNombreCompleto)
+
+//8.    Crear una variable llamada textoPresentacion y guardar en ella un texto comprendido con todas las variables creadas hasta el momento.
+
+let textoPresentacion = "Mi nombre es " + miNombreCompleto + ". Tengo " + miEdad + " años." + "Mi mascota es " + miMascota + " y tiene " + edadMascota + " años."
+
+console.log(textoPresentacion)
+
+//9.    Crear una variable sumaEdades, restaEdades, productoEdades, divisionEdades y calcular sus respectivas operaciones con las variables miEdad y edadMascota.
+//10.    Realizar la carga de las variables del punto 1 al 5 utilizando prompt() y luego ejecutar el código correspondiente para realizar los puntos del 6 al 9 con los datos cargados.
+//11.    Crear un objeto llamado alumno con un mínimo de 5 propiedades, mostrar dicho objeto utilizando console.table() y también mostrar cada una de las propiedades del objeto por separado.
+//12.    Crear un objeto llamado mascota con un mínimo de 5 propiedades, mostrar dicho objeto utilizando console.table() y también mostrar cada una de las propiedades del objeto por separado.
